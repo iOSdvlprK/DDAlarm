7e7267f0aea4c56f0ac2445d5cf46893962e8941
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-03-06 21:57:46 +0900

    Initial Commit

---
+++ DDAlarm/DDAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDAlarmApp.swift
+//  DDAlarm
+//
+//  Created by joe on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct DDAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
