fa5729d80c505be7cf9893c58f79f41fdd2a164a
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-03-06 23:08:16 +0900

    create the app icon

--- DDAlarm/DDAlarmApp.swift
+++ DDAlarm/DDAlarmApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DDAlarmApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainAlarmView()
         }
     }
 }
--- DDAlarm/MainAlarmView.swift
+++ DDAlarm/MainAlarmView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainAlarmView.swift
 //  DDAlarm
 //
 //  Created by joe on 3/6/25.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainAlarmView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    MainAlarmView()
 }
