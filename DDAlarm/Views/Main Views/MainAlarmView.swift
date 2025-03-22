d74a3518c4ca8da83a61b6df2b7f804cb01713ab
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-03-22 23:36:05 +0900

    back to the main alarm view

--- DDAlarm/Views/Main Views/MainAlarmView.swift
+++ DDAlarm/Views/Main Views/MainAlarmView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct MainAlarmView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            AddEditAlarmView(currentAlarmIndex: nil, alarmModel: .DefaultAlarm())
+                .tabItem {
+                    Label("Alarms", systemImage: "alarm.fill")
+                }
+            
+            AboutView()
+                .tabItem {
+                    Label("About", systemImage: "info.circle.fill")
+                }
         }
-        .padding()
     }
 }
 
