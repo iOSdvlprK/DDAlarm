c0d19d16e514507a95ca70942487091ed1965b3e
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-03-26 23:56:49 +0900

    circular time view computed properties

--- DDAlarm/Views/Supporting Views/Circular Time View/CentralDatePickerView.swift
+++ DDAlarm/Views/Supporting Views/Circular Time View/CentralDatePickerView.swift
@@ -13,6 +13,16 @@ struct CentralDatePickerView: View {
     
     var lineWidth: CGFloat = 10.0
     
+    var startTime: Date { alarmModel.start }
+    var endTime: Date { alarmModel.end }
+    var percentDifference: CGFloat {
+        let value = dateToPercent(date: endTime) - dateToPercent(date: startTime)
+        return value >= 0 ? value : 1 + value
+    }
+    var startDateToPercent: CGFloat { dateToPercent(date: startTime) }
+    var endDateToPercent: CGFloat { startDateToPercent + percentDifference }
+    var rotateCircleOffset: CGFloat { 360 * startDateToPercent }
+    
     var body: some View {
         Circle()
             .stroke(lightGray, lineWidth: lineWidth)
