9578bf7b6c8382d94df166f6ee09eb54b5c2a7f3
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-03-29 23:38:05 +0900

    MainAddEditAlarmView

---
+++ DDAlarm/Views/Main Views/MainAddEditAlarmView.swift
@@ -0,0 +1,36 @@
+//
+//  MainAddEditAlarmView.swift
+//  DDAlarm
+//
+//  Created by joe on 3/29/25.
+//
+
+import SwiftUI
+
+struct MainAddEditAlarmView: View {
+    let currentAlarmIndex: Int?
+    @State var alarmModel: AlarmModel
+    
+    @State private var selectedTab = "One"
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            AddEditAlarmView(currentAlarmIndex: currentAlarmIndex, alarmModel: alarmModel)
+                .tag("One")
+            
+            AddEditCircularAlarmView(currentAlarmIndex: currentAlarmIndex, alarmModel: alarmModel)
+                .tag("Two")
+        }
+        .onAppear() {
+            UIPageControl
+                .appearance()
+                .currentPageIndicatorTintColor = .gray
+        }
+        .tabViewStyle(.page(indexDisplayMode: .always))
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    MainAddEditAlarmView(currentAlarmIndex: nil, alarmModel: .DefaultAlarm())
+}
